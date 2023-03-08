f3f923a2c7cd07c7eaea2f1f2d0ce7da4d878ee1
SebastianTylicki <sebastian.tylicki@onet.pl>
2023-03-08 17:08:43 +0100

    lab1

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ print(listy)
 tab.sort()
 print(tab)
 
-slownik = {'a':2, 1: 2, 4:'ab', 1:3}
+slownik = {'a': 2, 4: 'ab', 1: 3}
 print(slownik)
 print(slownik[4])
 slownik['klucz'] = 'wartosc'
@@ -66,3 +66,89 @@ slownik.pop('klucz')
 print(slownik)
 print(slownik.keys())
 print(slownik.values())
+
+print('a=%(zm)d' % {'zm': 12})
+napis = 'a={}, b={}'
+print(napis.format(12, 12))
+
+# if warunek:
+    # instrukcja 1
+    # instrukcja 2
+# elif warunek 2:
+    # instrukcja 1
+    # instrukcja 2
+# else:
+    # instrukcja 1
+
+# x = input('Podaj x: ')
+# y = input('Podaj y: ')
+#  print(x)
+#  print(y)
+#  print(type(x))
+#  print(type(y))
+# x = int(x)
+# y = int(y)
+#  print(type(x), type(y))
+#  if x > y:
+#      print(x)
+#  elif x < y:
+#      print(y)
+#  else:
+#      print('x = y')
+
+# z = input('Podaj Z: ')
+# q = input('Podaj Q: ')
+# q = int(q)
+# z = int(z)
+#
+# if (x > y) & (z > q):
+#     print('x wieksze od y i z jest wieksze q')
+# else:
+#     print('x nie jest wieksze od y lub z nie jest wieksze od q')
+
+# for element in sekwencja:
+    # instrukcja 1
+    # instrukcja 2
+# else:
+    # instrukcja 1
+    # instrukcja 2
+
+print('---')
+
+for i in listy:
+    print(i)
+
+for j in range(0, len(listy)):
+    print(listy[j])
+
+# while warunek:
+    # instrukcja 1
+    # instrukcja 2
+# else:
+    # instrukcja
+
+print('Pętla while')
+
+licznik = 0
+while licznik != len(listy):
+    print(listy[licznik])
+    licznik += 1
+
+# licznik2 = 0
+# k = int(input('Podaj k: '))
+# while licznik2 != len(tab):
+#     if k - tab[licznik2] == 0:
+#         print('{} - {} = 0'.format(k, tab[licznik2]))
+#         break
+#     licznik2 += 1
+
+licznik3 = 0
+liczby = [1, 2, 2, 2, 4, 5, 6]
+
+print('Zadanie ')
+
+while licznik3 != len(liczby):
+        while liczby[licznik3] == 2:
+            liczby.pop(licznik3)
+        licznik3 += 1
+print(liczby)

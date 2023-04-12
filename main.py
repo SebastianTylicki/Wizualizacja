67947e278a4e82118f210ba706b08a77cdcce4d9
SebastianTylicki <115147343+SebastianTylicki@users.noreply.github.com>
2023-04-12 17:04:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -455,3 +455,106 @@ zad2()
 zad3(slownik)
 zad4()
 zad5()
+
+import numpy as np
+
+# inicjalizacja tablic
+a = np.array([[0, 1], [2, 3]])
+print(a)
+# drugi sposób
+a = np.arange(2, 5, 0.1)
+print(a)
+# wypisywanie typu zmiennej
+print(type(a))
+# sprawdzenie typu danych tablicy
+print(a.dtype)
+# inicjalizacja tablicy z konkretnym typem danych
+a = np.arange(2, dtype='int64')
+print(a.dtype)
+# zapisywanie kopii tablicy jako tablicy z innym typem
+b = a.astype('float')
+print(b)
+print(b.dtype)
+# wypisanie rozmiaru tablicy
+print(b.shape)
+# można też sprawdzić ilość wymiarów tablicy
+print(a.ndim)
+# stworzenie tablicy wielowmiarowej może wyglądać tak
+# parametrem przekazywanym do funkcji array jest obiekt
+# może to być Pythonowa lista
+m = np.array([np.arange(2), np.arange(2)])
+print(m.shape)
+# ponownie typem jest ndarray
+print(type(m))
+
+zera = np.zeros((5, 5))
+jedynki = np.ones((4, 4))
+print(zera)
+print(jedynki)
+# domyślny typ danych
+print(zera.dtype)
+print(jedynki.dtype)
+# można również stworzyć "pustą" macierz o podanych wymiarach
+# wartości umieszczane są losowo
+pusta = np.empty((2, 2))
+print(pusta)
+
+macierz = np.array([[12, 11], [2, 1]])
+print(macierz)
+# odwołanie do elementu
+poz_1 = macierz[1, 1]
+poz_2 = macierz[0][1]
+print(poz_1)
+print(poz_2)
+
+liczby_lin = np.linspace(1, 2, 5, endpoint=False)
+print(liczby_lin)
+z = np.indices((5, 3))
+print(z)
+mat_diag = np.diag([a for a in range(5)])
+print(mat_diag)
+z = np.fromiter(range(5), dtype='int32')
+print(z)
+
+znaki = b'ogolna'
+z1 = np.frombuffer(znaki, dtype='S1')
+print(z1)
+z2 = np.frombuffer(znaki, dtype='S2')
+print(z2)
+znaki = 'ogolna'
+z3 = np.array(list(znaki))
+z4 = np.array(list(znaki), dtype='S1')
+z5 = np.array(list(b'ogolna'))
+z6 = np.fromiter(znaki, dtype='S1')
+print(z3)
+print(z4)
+print(z5)
+print(z6)
+
+a = np.arange(10)
+print(a)
+s = slice(2, 7, 2)
+print(a[s])
+s = range(2, 7, 2)
+print(a[s])
+print(a[2:7:2])
+print(a[1:])
+print(a[2:5])
+
+mat = np.arange(25)
+mat = mat.reshape((5, 5))
+print(mat)
+print(mat[1:]) # od 2 wiersza
+print(mat[:, 1:2]) # 2 kolumna jako wektor
+print(mat[:, -1]) # ostatania kolumna
+print(mat[2:5, 1:3]) # 2 i 3 kolumna dla 3, 4, 5 wierszy
+print(mat[:, [2, 4]]) # 3 i 5 kolumna
+print('')
+
+x = np.array([[0, 1, 2], [3, 4, 5], [6, 7, 8], [9, 10, 11]])
+print(x)
+rows = np.array([[0, 0], [3, 3]])
+cols = np.array([[0, 2], [0, 2]])
+y = x[rows, cols]
+print(y)
+
